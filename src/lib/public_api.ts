34dfb7f06993e26c6d300404935bc7d11d0e6417
Zozoleclown <Zozoleclown1@gmail.com>
2019-01-18 02:00:11 +0100

    Updated to export everything a user may need

--- src/lib/public_api.ts
+++ src/lib/public_api.ts
@@ -1,1 +1,4 @@
-export * from './ngx-countdown.module';
+export { NgxCountdownModule } from './ngx-countdown.module';
+export { NgxCountdownComponent } from './components/ngx-countdown/ngx-countdown.component';
+export { NgxCountdownNotifyEvent } from './models/notify-event.model';
+export { NgxCountdownConfig } from './models/config.model';
